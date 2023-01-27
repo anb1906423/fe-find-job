60f99c74ee5b1884eb66c0f38304c64d3ea55df9
Truong Son 31 <truongsonpt.80@gmail.com>
2023-01-27 11:39:42 +0700

    fe push

--- store/reducers/userReducer.js
+++ store/reducers/userReducer.js
@@ -7,32 +7,6 @@ const initialState = {
 
 const userReducer = (state = initialState, action) => {
     switch (action.type) {
-        // case actionTypes.USER_LOGIN_SUCCESS:
-        //     return {
-        //         ...state,
-        //         isLoggedIn: true,
-        //         userInfo: action.userInfo,
-        //         // userPersist: action.userInfo,
-        //     };
-        // case actionTypes.UPDATE_USER_SUCCESS:
-        //     return {
-        //         ...state,
-        //         userInfo: action.data,
-        //     };
-        // case actionTypes.USER_LOGIN_FAIL:
-        //     return {
-        //         ...state,
-        //         isLoggedIn: false,
-        //         userInfo: null,
-        //     };
-        // case actionTypes.PROCESS_LOGOUT:
-        //     return {
-        //         ...state,
-        //         isLoggedIn: false,
-        //         userInfo: null,
-        //         token: null,
-        //     };
-
         default:
             return state;
     }
