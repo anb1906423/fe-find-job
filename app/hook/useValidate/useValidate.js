d2cdeeba80633198adcfe490458ade446036e681
Truong Son 31 <truongsonpt.80@gmail.com>
2023-02-04 19:22:28 +0700

    update

--- app/hook/useValidate/useValidate.js
+++ app/hook/useValidate/useValidate.js
@@ -4,6 +4,10 @@ const useValidate = (arrayClone) => {
     let isValid = true;
 
     for (let i = 0; i < arrayClone.length; i++) {
+        if (arrayClone[i] === undefined) {
+            arrayClone[i] = '';
+        }
+
         if (arrayClone[i] === '' || arrayClone[i].length === 0) {
             isValid = false;
             alert('Bạn đã nhập thiếu trường !');
