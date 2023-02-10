64cb7a55c4676c6f00aef3134f9e38ac28a88dfb
Truong Son 31 <truongsonpt.80@gmail.com>
2023-02-10 21:48:15 +0700

    fix bug loading Header delay

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
 
+import Header from './Header';
 import Footer from './Footer';
-
-const Header = dynamic(() => import('./Header'), {
-    ssr: false,
-});
 
 const Layout = ({ children }) => {
     return (
