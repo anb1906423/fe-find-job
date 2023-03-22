456756c27f27db3aeb93adfe321e013977bcf8d4
anb1906423 <100342430+anb1906423@users.noreply.github.com>
2023-03-22 23:18:24 +0700

    home page

---
+++ components/CongViecComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CongViecComponent = (props) => {
+  return (
+    <div classsName='cong-viec-component'>
+      {props.chucDanh}
+    </div>
+  )
+}
+
+export default CongViecComponent
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Carousel from '../components/Carousel';
 import Heading from '../components/Heading';
+import CongViecComponent from '../components/CongViecComponent';
+import axios from 'axios'
+import { backendAPI } from '../config'
+import { swalert, swtoast } from '../mixins/swal.mixin'
 
 const index = () => {
+    const [jobs, setJobs] = useState([])
+
+    useEffect(() => {
+        const handleGetJob = async () => {
+            try {
+                const res = await axios.get(backendAPI + '/cong-viec')
+                setJobs(res.data)
+                console.log(jobs);
+            } catch (error) {
+                console.log(error);
+                swtoast.error({
+                    text: error
+                })
+            }
+        }
+
+        handleGetJob()
+    }, jobs)
+    console.log(jobs);
     return (
         <div className="trang-chu">
             <div className="carousel-group">
@@ -10,6 +33,18 @@ const index = () => {
             </div>
             <div className="chua-noi-dung">
                 <Heading tieuDe="Việc làm tốt nhất" />
+                <div className="the-best-job-wp">
+                    {
+                        jobs && jobs.map((job, index) => {
+                            return (
+                                <CongViecComponent
+                                    key={index}
+                                    chucDanh={job.chucDanh}
+                                />
+                            )
+                        })
+                    }
+                </div>
             </div>
         </div>
     );
