d1d54eecfd638d58811100ed75717251e9dfeb2e
Truongsondev <trongsonpt.80@gmail.com>
2023-04-16 13:53:41 +0700

    git add

--- components/UngTuyenComponent/components/UngVien/ungTuyenUngVien.js
+++ components/UngTuyenComponent/components/UngVien/ungTuyenUngVien.js
@@ -1,8 +1,14 @@
 import React, { useEffect } from 'react';
 import useGetRoleUser from '../../../../app/hook/useGetRoleUser/useGetRoleUser';
+import { roleUser } from '../../../../util/constant';
 
 export default function UngTuyenUngVien() {
     const role = useGetRoleUser();
+
+    useEffect(() => {
+        if (role && role === roleUser.UngVien) {
+        }
+    }, [role]);
 
     return <div>UngTuyenUngVien</div>;
 }
