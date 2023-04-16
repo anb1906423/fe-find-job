899ce27b5d05762a9882ac081e0a1132c6b1a391
Truongsondev <trongsonpt.80@gmail.com>
2023-04-16 14:03:21 +0700

    push

--- components/UngTuyenComponent/components/UngVien/ungTuyenUngVien.js
+++ components/UngTuyenComponent/components/UngVien/ungTuyenUngVien.js
@@ -1,12 +1,15 @@
 import React, { useEffect } from 'react';
 import useGetRoleUser from '../../../../app/hook/useGetRoleUser/useGetRoleUser';
 import { roleUser } from '../../../../util/constant';
+import { useRouter } from 'next/router';
 
 export default function UngTuyenUngVien() {
     const role = useGetRoleUser();
+    const router = useRouter();
 
     useEffect(() => {
-        if (role && role === roleUser.UngVien) {
+        if (role || role !== roleUser.UngVien) {
+            router.push('/404-not-found');
         }
     }, [role]);
 
