204a5b43c6c10bc7264b55c643371f5f4deeb022
Truong Son Dev <truongsonpt.80@gmail.com>
2023-05-03 20:53:52 +0700

    commit

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,6 +6,8 @@ import axios from 'axios';
 import { backendAPI } from '../config';
 import { swalert, swtoast } from '../mixins/swal.mixin';
 import { DoubleRightOutlined } from '@ant-design/icons';
+import { Row, Col } from 'react-bootstrap';
+import { Image } from 'antd';
 
 const index = () => {
     const [jobs, setJobs] = useState([]);
@@ -31,7 +33,7 @@ const index = () => {
                               idNhaTuyenDung: company.id,
                           }
                         : job;
-                    });
+                });
 
                 setJobs(jobsWithCompanyInfo);
             } catch (error) {
@@ -79,6 +81,83 @@ const index = () => {
                         </button>
                     </div>
                 )}
+            </div>
+            <div className="my-3">
+                <div>
+                    <hr></hr>
+                </div>
+                <Row>
+                    <Col md={8}>
+                        <p
+                            style={{
+                                textAlign: 'justify',
+                                color: '#666',
+                                fontSize: 14,
+                                fontStyle: 'italic',
+                            }}
+                        >
+                            Tôi rất vui được giới thiệu về trang web Find Job của bạn. Find Job là một trang web tìm
+                            kiếm việc làm. Trang web của bạn có thể giúp người dùng tìm kiếm việc làm phù hợp với nhu
+                            cầu của họ. Trang web Find Job được thiết kế để giúp người dùng tìm kiếm việc làm phù hợp
+                            với nhu cầu của họ. Trang web của bạn có thể giúp người dùng tìm kiếm việc làm theo nhiều
+                            tiêu chí khác nhau như địa điểm, lĩnh vực, mức lương và kinh nghiệm. Bạn có thể tìm kiếm
+                            việc làm theo từ khóa hoặc theo tên công ty. Trang web của bạn cũng cung cấp thông tin chi
+                            tiết về các công việc được đăng tải trên trang web của bạn. Ngoài ra, trang web Find Job
+                            cũng cung cấp cho người dùng các công cụ để tạo hồ sơ cá nhân và quản lý hồ sơ cá nhân của
+                            họ. Người dùng có thể tạo hồ sơ cá nhân của mình và đăng ký cho các công việc mà họ quan
+                            tâm. Trang web của bạn cũng cung cấp cho người dùng các công cụ để quản lý các ứng viên và
+                            các công việc đã đăng tải trên trang web của bạn. Tôi hy vọng trang web Find Job của bạn sẽ
+                            giúp ích cho nhiều người tìm kiếm việc làm. 😊
+                        </p>
+                    </Col>
+                    <Col md={4}>
+                        <Image className="rounded" src="./img/findjob.jpg" alt="image" />
+                    </Col>
+                </Row>
+            </div>
+            <div className="my-3">
+                <div>
+                    <hr></hr>
+                </div>
+                <Row>
+                    <Col md={4}>
+                        <iframe
+                            className="rounded"
+                            style={{
+                                width: '100%',
+                                height: 240,
+                            }}
+                            src="https://www.youtube.com/embed/y7tHZ3WeJAw?list=RDy7tHZ3WeJAw"
+                            title="Ruth B. - Dandelions (Official Lyric Video)"
+                            frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            allowfullscreen
+                        ></iframe>
+                    </Col>
+                    <Col md={8}>
+                        <p
+                            style={{
+                                textAlign: 'justify',
+                                color: '#666',
+                                fontSize: 14,
+                                fontStyle: 'italic',
+                            }}
+                        >
+                            Tôi rất vui được giới thiệu về trang web Find Job của bạn. Find Job là một trang web tìm
+                            kiếm việc làm. Trang web của bạn có thể giúp người dùng tìm kiếm việc làm phù hợp với nhu
+                            cầu của họ. Trang web Find Job được thiết kế để giúp người dùng tìm kiếm việc làm phù hợp
+                            với nhu cầu của họ. Trang web của bạn có thể giúp người dùng tìm kiếm việc làm theo nhiều
+                            tiêu chí khác nhau như địa điểm, lĩnh vực, mức lương và kinh nghiệm. Bạn có thể tìm kiếm
+                            việc làm theo từ khóa hoặc theo tên công ty. Trang web của bạn cũng cung cấp thông tin chi
+                            tiết về các công việc được đăng tải trên trang web của bạn. Ngoài ra, trang web Find Job
+                            cũng cung cấp cho người dùng các công cụ để tạo hồ sơ cá nhân và quản lý hồ sơ cá nhân của
+                            họ. Người dùng có thể tạo hồ sơ cá nhân của mình và đăng ký cho các công việc mà họ quan
+                            tâm. Trang web của bạn cũng cung cấp cho người dùng các công cụ để quản lý các ứng viên và
+                            các công việc đã đăng tải trên trang web của bạn. Tôi hy vọng trang web Find Job của bạn sẽ
+                            giúp ích cho nhiều người tìm kiếm việc làm. 😊
+                        </p>
+                    </Col>
+                </Row>
             </div>
         </div>
     );
